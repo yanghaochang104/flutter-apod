8576e879b1e9a149fe4ae0e22a8872fef7b2b8f8
ray.chang <yanghaochang1004@gmail.com>
2022-10-02 03:12:47 +0800

    feat(Day14): add ApodData model

---
+++ lib/model/apod_data.dart
@@ -0,0 +1,26 @@
+class ApodData {
+  final String title;
+  final String url;
+  final String mediaType;
+  final String desc;
+  final String date;
+
+  ApodData(this.title, this.url, this.mediaType, this.desc, this.date);
+
+  ApodData.fromJson(Map<String, dynamic> json)
+      : title = json['title'],
+        mediaType = json['media_type'],
+        url = json['media_type'] == 'image'
+            ? json['hdurl']
+            : json['thumbnail_url'], // 如果當天的天文資料是影片檔，則取用影片截圖資源
+        desc = json['explanation'],
+        date = json['date'];
+
+  Map<String, dynamic> toJson() => {
+        'title': title,
+        'url': url,
+        'media_type': mediaType,
+        'explanation': desc,
+        'date': date,
+      };
+}
