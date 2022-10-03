c21176b37c0d34f0c8a2a31b27be268034dde2fd
ray.chang <yanghaochang1004@gmail.com>
2022-10-03 15:14:43 +0800

    feat(Day18): add CalendarDayCell to calendarBuilder

--- lib/Pages/calendar_page.dart
+++ lib/Pages/calendar_page.dart
@@ -1,3 +1,4 @@
+import 'package:apod/widgets/calendar_day_cell.dart';
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
@@ -16,18 +17,34 @@ class _CalendarPageState extends State<CalendarPage> {
     DateTime current = DateTime.now();
     return Center(
         child: TableCalendar(
-      firstDay: DateTime.utc(current.year - 2, 01, 01),
-      lastDay: DateTime.utc(current.year + 2, 12, 31),
-      focusedDay: _focusedDay,
-      selectedDayPredicate: (day) {
-        return isSameDay(_selectedDay, day);
-      },
-      onDaySelected: (selectedDay, focusedDay) {
-        setState(() {
-          _selectedDay = selectedDay; // 選取的日期
-          _focusedDay = focusedDay; // 頁面停留的日期
-        });
-      },
-    ));
+            headerStyle: const HeaderStyle(formatButtonVisible: false),
+            firstDay: DateTime.utc(current.year - 2, 01, 01),
+            lastDay: DateTime.utc(current.year + 2, 12, 31),
+            focusedDay: _focusedDay,
+            selectedDayPredicate: (day) {
+              return isSameDay(_selectedDay, day);
+            },
+            onDaySelected: (selectedDay, focusedDay) {
+              setState(() {
+                _selectedDay = selectedDay; // 選取的日期
+                _focusedDay = focusedDay; // 頁面停留的日期
+              });
+            },
+            calendarBuilders: CalendarBuilders(
+                defaultBuilder: (context, day, focusedDay) =>
+                    Center(child: CalendarDayCell(day: day)),
+                selectedBuilder: (context, day, focusedDay) => Center(
+                    child: CalendarDayCell(
+                        day: day,
+                        decoration: BoxDecoration(
+                          color: Colors.blue[300],
+                          shape: BoxShape.circle,
+                        ))),
+                todayBuilder: (context, day, focusedDay) => CalendarDayCell(
+                    day: day,
+                    decoration: BoxDecoration(
+                      color: Colors.blue[100],
+                      shape: BoxShape.circle,
+                    )))));
   }
 }
