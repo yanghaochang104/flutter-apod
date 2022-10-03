46abd7263a168fb1bf37a745a6dd99a4984bb945
ray.chang <yanghaochang1004@gmail.com>
2022-10-03 17:15:06 +0800

    feat(Day15): add ListView to FavoritePage

--- lib/Pages/favorite_page.dart
+++ lib/Pages/favorite_page.dart
@@ -12,17 +12,38 @@ class _FavoritePageState extends State<FavoritePage> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: TextButton(
-          onPressed: () {
-            Navigator.push(context, MaterialPageRoute(builder: (context) {
-              return Scaffold(
-                  appBar: AppBar(
-                    title: const Text('Lunation Matrix'),
-                  ),
-                  body: const MainPage());
-            }));
-          },
-          child: const Text('Navigate to MyHome Page')),
+      child: ListView.builder(
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            height: 100,
+            padding: const EdgeInsets.all(5.0),
+            child: SizedBox(
+              height: 100,
+              child: InkWell(
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return Scaffold(
+                        appBar: AppBar(
+                          title: const Text('one of the title'),
+                        ),
+                        body: const MainPage());
+                  }));
+                },
+                child: const Card(
+                    elevation: 5.0,
+                    child: Center(
+                      child: Text(
+                        'I am Image Title',
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w500),
+                      ),
+                    )),
+              ),
+            ),
+          );
+        },
+        itemCount: 2,
+      ),
     );
   }
 }
