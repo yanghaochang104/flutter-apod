9da19871c815666969741cf2640998d7f186ed8f
ray.chang <yanghaochang1004@gmail.com>
2022-10-15 16:56:31 +0800

    feat(Day17): add consumer of FavoriteState into FavoritePage

--- lib/Pages/favorite_page.dart
+++ lib/Pages/favorite_page.dart
@@ -1,5 +1,9 @@
+import 'package:apod/model/favorite_state.dart';
 import 'package:apod/widgets/astro_picture.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../model/apod_data.dart';
 
 class FavoritePage extends StatefulWidget {
   const FavoritePage({super.key});
@@ -12,44 +16,63 @@ class _FavoritePageState extends State<FavoritePage> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: ListView.builder(
-        itemBuilder: (BuildContext context, int index) {
-          return Container(
-            height: 100,
-            padding: const EdgeInsets.all(5.0),
-            child: SizedBox(
-              height: 100,
-              child: InkWell(
-                onTap: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return Scaffold(
-                        appBar: AppBar(
-                          title: const Text('one of the title'),
-                        ),
-                        body: const AstroPicture(
-                          title: 'title of astro picture',
-                          pictureUrl: '',
-                          desc: 'desc',
-                          note: "don't know what to fill yet",
-                          isFavorite: false,
-                        )); // 要填什麼資料進去呢？？
-                  }));
-                },
-                child: const Card(
-                    elevation: 5.0,
-                    child: Center(
-                      child: Text(
-                        'I am Image Title',
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.w500),
+      child: Consumer<FavoriteState>(
+          // 透過context, 取用上層傳進來的Favorite Provider
+          builder: (context, favoriteState, child) {
+        List<ApodData> list = favoriteState.favoriteList;
+        return list.isNotEmpty
+            ? ListView.builder(
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    height: 100,
+                    padding: const EdgeInsets.all(5.0),
+                    child: SizedBox(
+                      height: 100,
+                      child: InkWell(
+                        onTap: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) {
+                            return Scaffold(
+                                appBar: AppBar(
+                                  title: Text(list[index].date),
+                                ),
+                                body: ChangeNotifierProvider.value(
+                                    // 使用.value constructor，沿用目前的favoriteState instance到下一頁
+                                    value:
+                                        favoriteState, // 目前包含在context之中的favoriteState instance
+                                    child:
+                                        AstroPicture(apodData: list[index])));
+                          }));
+                        },
+                        child: Card(
+                            elevation: 5.0,
+                            child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(list[index].date,
+                                      style: TextStyle(
+                                          fontSize: 12,
+                                          color: Colors.grey[400])),
+                                  Text(
+                                    list[index].title,
+                                    style: const TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w500),
+                                  ),
+                                ])),
                       ),
-                    )),
-              ),
-            ),
-          );
-        },
-        itemCount: 2,
-      ),
+                    ),
+                  );
+                },
+                itemCount: favoriteState.favoriteList.length,
+              )
+            : Center(
+                child: Text(
+                  '目前沒有收藏！',
+                  style: TextStyle(fontSize: 30, color: Colors.grey[400]),
+                ),
+              );
+      }),
     );
   }
 }
